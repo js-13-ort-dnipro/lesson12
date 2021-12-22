ff610516e61dcc936c0ee95d1c840afe8d67dcd6
Anatoliy Kigel <anatoliy.kigel@gmail.com>
2021-12-22 13:34:49 +0200

    Upload Files & Source

---
+++ src/chat-app/app.js
@@ -0,0 +1,23 @@
+
+    import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.26/vue.esm-browser.prod.min.js';
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+   
+    /*
+
+        const firebaseConfig = {
+            apiKey: "AIzaSyCvl4bW9CUbNxzk5AdWYMTlgZ8zmZE_5Qs",
+            authDomain: "education-project-b6f9d.firebaseapp.com",
+            projectId: "education-project-b6f9d",
+            storageBucket: "education-project-b6f9d.appspot.com",
+            messagingSenderId: "570591471946",
+            appId: "1:570591471946:web:d617811b243272f24539b3"
+        };
+        
+        initializeApp(firebaseConfig); // Init Firebase App
+
+        const db = getFirestore(); // Init Firestore DB
+
+    */
---
+++ src/demo-template/app.js
@@ -0,0 +1,16 @@
+
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
+
+    import { getFirestore } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js";
+   
+    /*
+    const firebaseConfig = {
+        apiKey: "AIzaSyCvl4bW9CUbNxzk5AdWYMTlgZ8zmZE_5Qs",
+        authDomain: "education-project-b6f9d.firebaseapp.com",
+        projectId: "education-project-b6f9d",
+        storageBucket: "education-project-b6f9d.appspot.com",
+        messagingSenderId: "570591471946",
+        appId: "1:570591471946:web:d617811b243272f24539b3"
+    };
+    */
+    
